a2e0c0fe9143e13a9ffcc02d21ba52d05706eae9
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-15 16:54:45 -0400

    Adding jokesapp

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pagesApp',
+    'jokes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pagesApp.urls')), 
+    path('', include('pagesApp.urls')),
+    path('jokes/', include('jokes.urls')), 
 ]
