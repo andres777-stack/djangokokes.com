551878f9b7f78d4836dc71a96ba0e45adf8b6a89
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-17 22:48:05 -0400

    Creating Joke Model

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutUsView
+from .views import JokeListView
 
 app_name = 'jokes'
 
 urlpatterns = [
-    
+    path('', JokeListView.as_view(), name = 'list'),
 ]
--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Joke
+
+class JokeListView(ListView):
+    model = Joke
+    
 
 # Create your views here.
