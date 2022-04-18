2a037a98c5436329b42f4cf2966c27200706962f
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-18 14:39:46 -0400

    Adding createView and UpdateView

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Joke
 
 class JokeListView(ListView):
@@ -6,5 +6,13 @@ class JokeListView(ListView):
 
 class JokeDetailView(DetailView):
     model = Joke
-    
+
+#This view is for created a new object
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
 # Create your views here.
