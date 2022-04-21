aeb8c37b86f8bd07703cd51d48695a6f0f7a0b86
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-20 22:50:37 -0400

    adding document generator

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'pagesApp',
     'jokes',
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
