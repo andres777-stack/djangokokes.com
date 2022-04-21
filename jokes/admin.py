b4bc5889fa6069a176bd2c26c1ca36066ab427ac
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-20 22:34:02 -0400

    Adding jokemodels to admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Joke
 
+@admin.register(Joke)
+class Joke(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'answer', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: #editing an existint object
+            return ('created', 'updated')
+        
+        return ()
 # Register your models here.
