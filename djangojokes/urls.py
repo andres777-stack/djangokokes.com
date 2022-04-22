3158ccda1ef97fcb26513b2757545fec44740dbd
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-21 23:10:41 -0400

    adding job application form with email

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pagesApp.urls')),
     path('jokes/', include('jokes.urls')), 
+    path('jobs/', include('jobs.urls')),
 ]
