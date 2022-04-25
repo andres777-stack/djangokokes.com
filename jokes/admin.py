917f2aa236a273cf0d2be60be1e7cf916fc49344
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-25 15:07:23 -0400

    adding categorymodel to admin and to Jokeform

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Joke
+from .models import Category, Joke
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated'] 
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('slug', 'created', 'updated')
+        return ()
 
 @admin.register(Joke)
 class Joke(admin.ModelAdmin):
@@ -11,4 +21,6 @@ class Joke(admin.ModelAdmin):
             return ('slug', 'created', 'updated')
         
         return ()
+
+
 # Register your models here.
