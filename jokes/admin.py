ac26510e9712fe6ebdc2a090655dcb82483011c4
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-25 15:35:21 -0400

    Adding tag with manytomanyfields

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Category, Joke
+from .models import Category, Tag, Joke
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
     list_display = ['category', 'created', 'updated'] 
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('slug', 'created', 'updated')
+        return ()
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated'] 
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
