d283ccf68f3020571a918fc3c2cb418947339a4a
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-25 13:09:24 -0400

    adding validations

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,11 @@
 from datetime import datetime
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+
+def validate_future_date(value):
+    if value < datetime.now().date():
+        raise ValidationError(message=f'{value} is in the past.', code='past_date')
 
 EMPLOYMENT_TYPE =(
     ("1", "Full-Time"),
@@ -21,10 +27,11 @@ class JobApplicationForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': True}))
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(widget=forms.URLInput(attrs={'size': '50', 
-    'placeholder': 'https://www.example.com'}))
+    website = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'pagina', 'size':'50'}), validators=[URLValidator(schemes=['http', 'https'])])
+    #website = forms.URLField(widget=forms.URLInput(attrs={'size': '50', 
+    #'placeholder': 'https://www.example.com'}))
     employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPE)
-    start_date = forms.DateField(widget=forms.SelectDateWidget(years=YEARS))
+    start_date = forms.DateField(widget=forms.SelectDateWidget(years=YEARS), validators=[validate_future_date])
     available_days = forms.TypedMultipleChoiceField(choices=AVAILABLE_DAYS, coerce=int, widget=forms.CheckboxSelectMultiple(attrs={'checked': True}))
     desire_hourly_wage = forms.DecimalField(widget=forms.NumberInput(attrs={'min': '10.10', 'max':'100.00', 'step':'.25'}))
     cover_letter = forms.CharField(max_length=50)
