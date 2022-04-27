0ce6bcdc7984ae3f13e4919f8e9967808ded5d10
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-26 23:42:30 -0400

    adding messages

--- pagesApp/views.py
+++ pagesApp/views.py
@@ -1,11 +1,21 @@
 #from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+    
+    #def get(self, request, *args, **kwargs):
+    #    messages.debug(request, 'Debug message')
+    #    messages.info(request, 'info message')
+    #    messages.success(request, 'success message')
+    #    messages.warning(request, 'warning message')
+    #    messages.error(request, 'error message')
+    #    return super().get(request, args, kwargs)
 
 
 # Create your views here.
--- users/views.py
+++ users/views.py
@@ -4,11 +4,13 @@ from django.views.generic import UpdateView
 from allauth.account.views import PasswordChangeView
 from .forms import CustomUserChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
-class MyAccountPageView(LoginRequiredMixin, UpdateView):
+class MyAccountPageView(SuccessMessageMixin,LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
     template_name = 'account/my_account.html'
+    success_message = 'Update Successful'
 
     def get_object(self):
         return self.request.user
