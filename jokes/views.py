3327c40707066ef2aa09ebd517ac8ea185b9d8ad
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-04-26 23:58:04 -0400

    adding messages to joke views

--- jokes/views.py
+++ jokes/views.py
@@ -3,6 +3,8 @@ from .models import Joke
 from django.urls import reverse_lazy
 from .forms import JokeForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 class JokeListView(ListView):
     model = Joke
@@ -11,17 +13,19 @@ class JokeDetailView(DetailView):
     model = Joke
 
 #This view is for created a new object
-class JokeCreateView(LoginRequiredMixin, CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_messssage = 'Joke created.'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class JokeUpdateView(UserPassesTestMixin, UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke update'
 
     def test_func(self):
         obj = self.get_object()
